ce4b1632bbdda313d61dfbee95b0326c92fe3fa6
gouzmi <guillaume.dupont.rm@gmail.com>
2017-12-13 01:54:28 +0100

    tentative de connexion

--- site original/login.php
+++ site original/login.php
@@ -14,19 +14,19 @@
 		  <div class="page">
         <section>
 	       <p id = "titre"> Connexion </p> <br><br>
-            <form method="post" action="accueilConnecté(Par pièce).php"  id ="Login"><!-- début du formulaire-->
+            <form method="post" action="traitementlogin.php"  id ="Login"><!-- début du formulaire-->
 
-			<label for="email">Adresse email :
-			<input type="email" name="email"  id="mail" placeholder="Ex : super.client@gmail.com" required></label>
-			<br> <br>
-			<label for="password">Mot de passe :
-    		<input type="password" name="pass" id="password" required ></label>
-    	    </form>
-    		<br><br><br>
-
-    		<center><input type="submit" value="Accéder">
-            <input type="submit" value="Domisep"></center>
-
+								<label for="email">Adresse email :
+								<input type="email" name="email"  id="mail" placeholder="Ex : super.client@gmail.com" required></label>
+								<br> <br>
+								<label for="password">Mot de passe :
+					    	<input type="password" name="pass" id="password" required ></label>
+								<br><br><br>
+								<center>
+									<input type="submit" value="Accéder">
+										<!--<input type="submit" value="Domisep">-->
+								</center>
+    	    	</form>
     		<br><br><br>
             <div id="inscription"><a href="formulaire.php"> Inscription</a></div>
             <div id="oubli"><a href="mdp_oublie.php"> Mot de passe oublié ?</a></div>
