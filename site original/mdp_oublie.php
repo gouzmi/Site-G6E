cc80c18017d7d26f7c197deeccb01feab8869b4d
DarleneEustache <darlene.eustache@gmail.com>
2017-12-15 01:01:52 +0100

    Modif

--- site original/mdp_oublie.php
+++ site original/mdp_oublie.php
@@ -17,14 +17,14 @@
 	       <p id = "titre"> Récupération de votre mot de passe </p> <br><br>
             <form method="post" action="traitementmdp_oublie.php"  id ="mdp_oublie">
 
-								<label for="email">Quel est votre animal préféré ? :</label>
+								<label for="email">Quel est votre animal préféré ? </label>
 								<br><br>
 
 								<input type="animal" name="animal"  id="animal" placeholder="Ex : requin, serpent..." required>
 
 								<br><br>
 								<center>
-									<input type="submit" value="Envoyer">
+									<input type="submit" value="Envoyer" id="envoyer">
 								</center>
     	    	</form>
     		<br><br>
