f3b321a336803356bf0241fbbdae7f4479d0ac4b
gouzmi <guillaume.dupont.rm@gmail.com>
2017-12-22 16:57:09 +0100

    hashage

--- MvcModel/Models/formulaireModels.php
+++ MvcModel/Models/formulaireModels.php
@@ -10,8 +10,9 @@
 
       if ($_POST['mdp'] == $_POST['mdp2'])
       {
+        $mdp = password_hash($_POST['mdp'],PASSWORD_BCRYPT);
         $insert = $bdd->prepare("INSERT INTO utilisateur(nom, prenom, mail, mdp) VALUES(?, ?, ?, ?)");
-        $insert->execute(array($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['mdp'])) ;
+        $insert->execute(array($_POST['nom'], $_POST['prenom'], $_POST['mail'], $mdp)) ;
         $erreur= "Votre compte a bien été crée <a href=\"login.php\"> Se connecter</a> ";
       }
       else {
