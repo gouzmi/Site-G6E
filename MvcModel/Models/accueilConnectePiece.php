19a09e6abcd7cf8dec109d3dac12222ec81ab29a
DarleneEustache <darlene.eustache@gmail.com>
2018-01-08 15:29:06 +0100

    Modif accueilconnectepiece

--- MvcModel/Models/accueilConnectePiece.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Yuqing
- * Date: 22/12/2017
- * Time: 11:03
- */
--- MvcModel/javaScript/accueilConnectePiece.js
+++ MvcModel/javaScript/accueilConnectePiece.js
@@ -1,22 +1,20 @@
-function statut(id){
-    if(id == "SalleDeBain"){
-        document.getElementById("pageStatut").innerHTML = "10";
-    }else if (id == "SalleDeSejour"){
-        document.getElementById("pageStatut").innerHTML = "20";
-    }else if (id == "Salon"){
-        document.getElementById("pageStatut").innerHTML = "30";
-    }else if (id == "SalleManger"){
-        document.getElementById("pageStatut").innerHTML = "40";
-    }else if (id == "Chambre"){
-        document.getElementById("pageStatut").innerHTML = "50";
-    }else if (id == "Terrasse"){
-        document.getElementById("pageStatut").innerHTML = "60";
-    }else if (id == "Cuisine"){
-        document.getElementById("pageStatut").innerHTML = "70";
-    }else if (id == "Bureau"){
-        document.getElementById("pageStatut").innerHTML = "80";
-    }else if (id == "Garage"){
-        document.getElementById("pageStatut").innerHTML = "90";
+function openPiece(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
 
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
