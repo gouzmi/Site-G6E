5b635da40216001e6d50b89f90413ee31f967162
brunolebg <bruno-der@hotmail.fr>
2018-01-10 17:09:17 +0100

    yow

---
+++ MvcModel/Controlers/commentaire.php
@@ -0,0 +1,10 @@
+<?php session_start();
+if (isset($_SESSION['id'])) {
+    require('../Models/commentaireModels.php');
+
+    require('../Views/commentaireView.php');
+}
+
+else {
+  header("Location: login.php");
+}
---
+++ MvcModel/Models/commentaireModels.php
@@ -0,0 +1,15 @@
+<?php
+
+    include('connexiondb.php');
+
+// Récupération du billet
+    $req = $bdd->prepare('SELECT id_billet, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets WHERE id = ?');
+    $req->execute(array($_GET['billet']));
+    $donnees = $req->fetch();
+
+    $req->closeCursor(); // Important : on libère le curseur pour la prochaine requête
+
+// Récupération des commentaires
+$req = $bdd->prepare('SELECT auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
+$req->execute(array($_GET['billet']));
+?>
