cb5548088f703a76c76f9f5d4453c35f0573387b
Yuuqing <yuqing.zhu8028@gmail.com>
2018-01-11 12:05:42 +0100

    nouveau version editerMaison

--- MvcModel/Views/modifierCapteurView.php
+++ MvcModel/Views/modifierCapteurView.php
@@ -6,26 +6,33 @@
   <link rel="stylesheet" type="text/css" href="../Css/headerfooterr.css">
   <link rel="stylesheet" type="text/css" href="../Css/editerMaison.css">
 </head>
+
+
 <?php include ("header.php")?>
+
+
 <body class="no" id="menu">
+
+
   <div id="corps">
   <?php include("../Views/slideView.php") ;?>
   <div class="page">
-    <section>
-     <p id="titre">Modifier vos capteurs </p> <br><br>
-     <form action="" method="post">
+
+
+     <form action="" method="post" class="form-style-6">
+       <h1>Modifier votre capteur ici !</h1>
        <center>
-       <label id="question">Vous voulez ajouter un autre capteur ou supprimer un capteur? </label>
-       <br><br>
-       <input type="radio" name="ajouter" value="ajouter" id="ajouter"><label for="ajouter" >Ajouter un autre capteur</label>
-       <input type="radio" name="supprimer" value="supprimer" id="supprimer"><label for="supprimer">Supprimer un capteur</label>
-       <br><br><br>
+       <label>Vous voulez ajouter un autre capteur ou supprimer un capteur? </label>
+       <br><br><br><br>
+       <input type="radio" name="ajouter" value="ajouter" id="ajouter"><label class="ca" for="ajouter" >Ajouter un autre capteur</label>
+       <input type="radio" name="supprimer" value="supprimer" id="supprimer"><label class="ca" for="supprimer">Supprimer un capteur</label>
+       <br><br><br><br>
        <input type="submit" value="Valider" id="Valider">
      </center>
-     </form>
+     
      <div id="retour"><a href="editerMaison.php"> Retour </a></div>
+    </form>
      <br>
-   </section>
 </div>
 
 </div>
