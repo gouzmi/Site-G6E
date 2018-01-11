f7370c71edc2e1fd0fe2f4919dd2fea05b2f8eba
gouzmi <guillaume.dupont.rm@gmail.com>
2018-01-11 11:09:27 +0100

    admin et table préco, importez làà

--- MvcModel/Models/adminModels.php
+++ MvcModel/Models/adminModels.php
@@ -22,4 +22,15 @@
       $info3 = "Succès";
     }
 
+  $suppression = $bdd->query('SELECT * FROM utilisateur');
+
+  if (isset($_POST['suppclient2']))
+    {
+      $suppcl = $bdd->prepare("DELETE FROM `utilisateur` WHERE mail=? ");
+      $suppcl->execute(array($_POST['suppmailclient2']));
+      $info4 = "Succès";
+    }
+
+
+
  ?>
