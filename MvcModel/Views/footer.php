5d3b4267b68ab88a410630f2e22e99260717c8ab
DarleneEustache <darlene.eustache@gmail.com>
2018-01-18 10:23:08 +0100

    adresse footer connectée à la bdd

--- MvcModel/Views/footer.php
+++ MvcModel/Views/footer.php
@@ -1,7 +1,8 @@
+<?php require('../Models/footerModel.php'); ?>
 <footer>
     <ul>
-        <li>© HomeTech2017. Tous droits réservés</li>
-        <li>Localisation : 28 rue Notre-Dame des Champs, 75006, PARIS </li>
+        <li>© HomeTech2018. Tous droits réservés</li>
+        <?php adressedomhome($bdd); ?>
         <li><a href="contact.php" class="link"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
     </ul>
 </footer>
