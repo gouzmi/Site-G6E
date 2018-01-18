31beec12842f3f28e37d6704c739143840baf98f
gouzmi <guillaume.dupont.rm@gmail.com>
2018-01-18 11:52:30 +0100

    FORUM

--- site original/commentaire.php
+++ site original/commentaire.php
@@ -16,14 +16,14 @@
 
     <?php
     // Connexion à la base de données
-      try
-      {
-	       $bdd = new PDO('mysql:host=localhost;dbname=test2;charset=utf8', 'root', '');
-       }
-      catch(Exception $e)
-      {
-        die('Erreur : '.$e->getMessage());
-      }
+        try
+        {
+          $bdd = new PDO('mysql:host=localhost;dbname=test2;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+                die('Erreur : '.$e->getMessage());
+        }
 
     // Récupération du billet
         $req = $bdd->prepare('SELECT id_billet, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets WHERE id = ?');
@@ -34,7 +34,7 @@
       <div class="news">
         <h3>
           <?php echo htmlspecialchars($donnees['titre']); ?>
-          <em>le <?php echo $donnees['date_creation']; ?></em>
+          <em>le <?php echo $donnees['date_creation_fr']; ?></em>
         </h3>
       </div>
 
@@ -44,7 +44,7 @@
           $req->closeCursor(); // Important : on libère le curseur pour la prochaine requête
 
 // Récupération des commentaires
-$req = $bdd->prepare('SELECT auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
+$req = $bdd->prepare('SELECT * FROM commentaires WHERE id_billet = ? ORDER BY date_creation');
 $req->execute(array($_GET['billet']));
 
 while ($donnees = $req->fetch())
@@ -52,7 +52,7 @@ while ($donnees = $req->fetch())
 ?>
 
 
-<p><strong><?php echo htmlspecialchars($donnees['auteur']); ?></strong> le <?php echo $donnees['date_commentaire_fr']; ?>
+<p><strong><?php echo htmlspecialchars($donnees['auteur']); ?></strong> le <?php echo $donnees['date_creation']; ?>
 <br><br>
 <?php echo nl2br(htmlspecialchars($donnees['commentaire'])); ?></p>
 </div>
@@ -68,8 +68,8 @@ $req->closeCursor();
                     <label for = "auteur"><strong>Votre pseudo :</strong></label>
                     <input type = "text" name = "auteur" id = "auteur"/><br/>
                     <label for = "commentaire"><strong>Commentaire :</strong></label><br/>
-                    <textarea type ="text" name = "commentaire" id = "commentaire" rows ="8" cols="45">Votre commentaire...</textarea><br/>
-                    <input type = "submit" value = "Poster Votre Commentaire"/>
+                    <textarea type ="text" name = "commentaire" id = "commentaire" rows ="8" cols="45" placeholder="Votre commentaire..." required></textarea><br/>
+                    <input type = "submit" value = "Poster votre commentaire"/>
                 </p>
             </form>
 </body>
--- site original/commentaires_post.php
+++ site original/commentaires_post.php
@@ -18,22 +18,20 @@
 // Connexion à la base de données
 try
 {
-    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-    $bdd = new PDO('mysql:host=localhost;dbname=test2', 'root', '', $pdo_options);
+    $bdd = new PDO('mysql:host=localhost;dbname=test2', 'root', '');
+    $bdd->exec("SET CHARACTER SET utf8");
 }
-catch(Exception $e)
+catch (Exception $e)
 {
-    die('Erreur : '.$e->getMessage());
+    die('Erreur : ' . $e->getMessage());
 }
+
+
 if(!empty($_GET['billet']) AND !empty($_POST['auteur']) AND !empty($_POST['commentaire']))
 {
  // Insertion du message à l'aide d'une requête préparée
-    $req = $bdd->prepare('INSERT INTO commentaires(id_billet, auteur, commentaire, date_creation) VALUES(:id_billet, :auteur, :commentaire, NOW())') or die(print_r($bdd -> getMessage()));
-    $req->execute(array(
-                         ':id_billet' => htmlspecialchars($_GET['billet']),
-                         ':auteur' => htmlspecialchars($_POST['auteur']),
-                         ':commentaire' => htmlspecialchars($_POST['commentaire'])));
-              // Redirection du visiteur vers la page des commentaires
+    $req = $bdd->prepare("INSERT INTO `commentaires`( `id_billet`, `auteur`, `commentaire`) VALUES (?,?,?)");
+    $req->execute(array($_GET['billet'], $_POST['auteur'], $_POST['commentaire']));
               header('Location:commentaire.php?billet=' . $_GET['billet']);
 }
     ?>
