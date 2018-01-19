e021f1deb0abb4327afe4131209c4e9467183a3a
Yuuqing <yuqing.zhu8028@gmail.com>
2018-01-19 10:44:18 +0100

    ...

--- MvcModel/Models/ajouterPieceModel.php
+++ MvcModel/Models/ajouterPieceModel.php
@@ -12,21 +12,22 @@
 
     $resultat = filter_input_array(INPUT_POST, $filter_def);
 
-
-    print_r($resultat);// ca marche jusqu'a ici!!!!!
     //Données nettoyées
     $nom = $resultat['nom'];
     $superficie = $resultat['superficie'];
 
-    if ((preg_match('#^[A-Za-zÀ-ÖØ-öø-ÿ-]+$#', $nom)) == false)
-    {
-        $erreur= 'Le nom ne doit contenir que des lettres' ;// ca marche jusqu'a ici!!!!!
-    }
-    else
-    {
-      $ajoutPie = $bdd->prepare("INSERT INTO piece (id_type_piece, id_logement, nom_piece, superficie_piece) VALUES (1, 2,'ops',24)");
-      $ajoutPie->execute();
+    print_r(array($nom,$superficie,$_POST['varietePie']));// ca marche jusqu'a ici!!!!!
+
+    //if ((preg_match('#^[A-Za-zÀ-ÖØ-öø-ÿ-]+$#', $nom)) == false)
+    //{
+    //    $erreur= 'Le nom ne doit contenir que des lettres' ;// ca marche jusqu'a ici!!!!!
+    //}
+    //else
+    //{
+      $ajoutPie = $bdd->prepare("INSERT INTO piece(id_type_piece,nom_piece,superficie_piece) VALUES(?,?,?)");
+      $ajoutPie->execute(array($_POST['varietePie'],$nom,$superficie));
+
       $info = "Votre pièce a été bien ajouté !";
-    }
+    //}
   }
 ?>
--- MvcModel/Views/profilView.php
+++ MvcModel/Views/profilView.php
@@ -27,9 +27,11 @@
             <?php  if (isset($erreur)) {
               echo $erreur;} ?>
           </center>
-          <form method="post" action="">
-          <input type="text" name="nom" placeholder="Nom" value="<?php echo $user['nom']?>"/>
-          <input type="text" name="prenom" placeholder="Prénom" value="<?php echo $user['prenom']?>" />
+          <form method="post" action="" id="Formulaire" name="Formulaire">
+          <span id='missNom'></span>
+          <input type="text" name="nom" placeholder="Nom" id="nom"  value="<?php echo $user['nom']?>"/>
+          <span id='missPrenom'></span>
+          <input type="text" name="prenom" placeholder="Prénom" id="prenom" value="<?php echo $user['prenom']?>" />
           <input type="text" name="adresse" placeholder="Adresse" value="<?php echo $user['adresse_contact']?>"/>
           <input type="text" name="cp" pattern="(^[0-9]{5}$)|(^2(A|B)[0-9]{3}$)"  placeholder="Code Postal" value="<?php echo $user['cp_contact']?>">
           <input type="text" name="ville" placeholder="Ville" value="<?php echo $user['ville_contact']?>">
@@ -37,10 +39,11 @@
           <input type="email" name="mail" placeholder="Email Address" value="<?php echo $user['mail']?>">
           <input type="password" name="mdp" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{6,}" required title="6 caractères minimun en majuscule et minuscule et un caractère spécial"  id="passe" placeholder="Mot de passe" value="">
           <input type="password" name="mdp2" required id="pass" placeholder="Confirmation de mot de passe">
-          <input type="submit" name="caseconditions" value="Modifier son profil" />
+          <input type="submit" name="caseconditions" id="inscrire" value="Modifier son profil" />
           </form>
     </div>
 
+    <script src="../javaScript/verificationFormulaire.js" type="text/javascript"></script>
 
 
 </div>
