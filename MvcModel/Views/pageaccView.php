b1b5e0cd62837a0f071070490be37bfd7acd19c1
Morgane-Eckert <morganeeckert@orange.fr>
2018-01-20 13:22:50 +0100

    début première page

---
+++ MvcModel/Views/pageaccView.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Page d'accueil</title>
+    <link rel="stylesheet" href="../Css/pageacc.css"/>
+    <link rel="stylesheet" href="../Css/headerfooterr.css"/>
+    </head>
+
+    <body>
+      <?php include("header.php") ?>
+
+    <div class="page">
+      <!--partie1-->
+          <section class="section s1">
+              <div><h1 class="titre">Notre entreprise : DomHome</h1>
+              </div>
+              <div class="p"><p>DomHome est un nouveau processus domotique innovant.
+                  Il vous permettra de gérer votre maison à distance et de garantir votre sécurité par l'intermédiaire de capteurs et actinneurs.<br>
+                  Par l'intermédiaire de DomHome, vous pourrez rendre votre maison, à la fois, connectée, sécurisée et personnalisée.</p>
+                  <br>
+
+              </div>
+          </section>
+      <!--partie2-->
+          <section class="section s2">
+            <div><h1 class="La maison connectée">La maison connectée</h1>
+            </div>
+            <div class="picandp"><div class="imgg" ><img src="../Images/bg-3.png"></div>
+            <div class="p">
+              <p>La « maison connectée » est une expression pour désigner un habitat où plusieurs éléments sont contrôlés à distance, éventuellement de manière automatisée : le chauffage, la lumière, les alarmes, etc.
+              La domotique alliée à la maison connectée permet de faire diminuer jusqu'à 40 % vos factures d'énergie : réglage du chauffage dans chaque pièce à l'heure voulue, capteur de présence pour gérer la température adéquate, capteur de fenêtre ouverte, programmation des volets (pour faire entrer la chaleur du soleil en hiver ou l'éviter en été...).</p>
+
+            </div>
+
+          </section>
+
+      <!--partie3-->
+          <section class="section s3">
+            <div><h1 class="Catalogue">Catalogue des capteurs</h1> </div>
+
+          </section>
