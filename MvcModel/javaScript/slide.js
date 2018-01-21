d0bc5fa1f9ce169f9d40d05d99f998d77762a529
gouzmi <guillaume.dupont.rm@gmail.com>
2018-01-21 22:17:51 +0100

    j'avais oublié ça

--- MvcModel/javaScript/slide.js
+++ MvcModel/javaScript/slide.js
@@ -4,10 +4,13 @@ function myFunction(x) {
     {
       document.getElementById('menu').className = "yes";
       document.getElementById('menu').style.backgroundColor = "rgba(0,0,0,0.4)";
+      document.getElementById("corps").style.marginLeft = "300px";
     }
     else
     {
       document.getElementById('menu').className = "no";
       document.getElementById('menu').style.backgroundColor = "";
+
+      document.getElementById("corps").style.marginLeft = "";
     }
 }
