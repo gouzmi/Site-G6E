d92c49de62058cc446400c4fb826b01cd7c02354
gouzmi <guillaume.dupont.rm@gmail.com>
2018-01-21 16:50:13 +0100

    fix faq probleme

--- MvcModel/Models/faqModel.php
+++ MvcModel/Models/faqModel.php
@@ -1,16 +1,5 @@
 <?php
-$req_question_faq = $bdd ->prepare('SELECT question_faq FROM faq where id_faq=?');
-$req_question_faq->execute(array($id_faq['id_faq']));
-while($question_faq = $req_question_faq ->fetch())
 
+require('connexiondb.php');
 
-{
-  echo  "<strong>".$question_faq['question_faq']."</strong>". '<br />';
-  $req_reponse_faq = $bdd->prepare('SELECT reponse_faq FROM faq where question_faq=?');
-  $req_reponse_faq->execute(array($question_faq['question_faq']));
-  while($reponse_faq = $req_reponse_faq->fetch())
-
-  {
-    echo "<p>".$reponse_faq['reponse_faq']."</p>".   '<br  />';
-  }
-?>
+$req_id_faq = $bdd->query('SELECT id_faq FROM faq');
--- MvcModel/Views/faqView.php
+++ MvcModel/Views/faqView.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 
 <html>
@@ -8,53 +7,54 @@
     <link rel="stylesheet" href="../Css/faq.css"/>
     <link rel="stylesheet" href="../Css/headerfooterr.css"/>
     <script src="https://use.fontawesome.com/3aa3fe383f.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="../Images/miniature.png" />
     </head>
 
+<body>
+  <?php include("header.php"); ?>
 
-    <?php include("header.php"); ?>
-    <body>
-      <h1 class="titre">Foire aux questions</h1>
-      <div class="page">
+    <div id="page1">
+
+
+        <h1 class="titre">Foire aux questions</h1>
         <section>
+          <?php
 
 
-<?php
+            while ($id_faq = $req_id_faq ->fetch())
+              {
+              ?>
+              <div class="question">
+                <?php
+                $req_question_faq = $bdd ->prepare('SELECT question_faq FROM faq where id_faq=?');
+                $req_question_faq->execute(array($id_faq['id_faq']));
+                while($question_faq = $req_question_faq ->fetch())
+                    {
+                      echo "<strong>".$question_faq['question_faq']."</strong>" . '<br />';
+                      $req_reponse_faq = $bdd->prepare('SELECT reponse_faq FROM faq where question_faq=?');
+                      $req_reponse_faq->execute(array($question_faq['question_faq']));
+                      while($reponse_faq = $req_reponse_faq->fetch())
+                          {
+                            echo  "<p>". $reponse_faq['reponse_faq']. "</p>".   '<br  />';
+                          }
+                    ?>
 
-try
-{
-		$bdd = new PDO('mysql:host=localhost; dbname=test2;charset=utf8',  'root', '');
-}
-catch(Exeption $e)
-{
-		die('Erreur: '.$e->getMessage());
-}
+                  </div>
+                    <?php
+                    }
+                ?>
+              <?php
+              }
+          $req_id_faq->closeCursor();
+          ?>
 
-$req_id_faq = $bdd->query('SELECT id_faq FROM faq');
-while ($id_faq = $req_id_faq ->fetch())
-
-{
-
-?>
-<question>
-	
-
-		<br />
-	<?php
-	}
-
-}
-
-
-$req_id_faq->closeCursor();
-
-?>
-</section>
-</div>
+        </section>
+      </div>
 </body>
 
 
-</body>
 <?php include("footer.php"); ?>
+</body>
 
 
 </html>
