decde10604cd8c9789e3fbb5aa58e5752cac77e5
gouzmi <guillaume.dupont.rm@gmail.com>
2018-01-22 17:10:10 +0100

    pb forum résolu

--- MvcModel/Models/billetModels.php
+++ MvcModel/Models/billetModels.php
@@ -6,8 +6,8 @@
   if(isset($_POST['action']))
   {
    // Insertion du message à l'aide d'une requête préparée
-      $req = $bdd->prepare("INSERT INTO `billets`(`titre`, `contenu`) VALUES (?,?)");
-      $req->execute(array($_POST['titre'], $_POST['contenu']));
+      $req2 = $bdd->prepare("INSERT INTO `billets`(`titre`, `contenu`) VALUES (?,?)");
+      $req2->execute(array($_POST['titre'], $_POST['contenu']));
       header('Location:billet.php');
   }
 
