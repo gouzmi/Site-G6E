cab844777b9d0369126333efa2a2a421f9079fa3
Morgane-Eckert <morganeeckert@orange.fr>
2018-01-24 15:31:54 +0100

    verif profil OK

--- MvcModel/Views/profilView.php
+++ MvcModel/Views/profilView.php
@@ -11,7 +11,6 @@
 </head>
 
 
-
 <body class="no" id="menu">
   <?php include("header.php") ?>
 
@@ -29,33 +28,31 @@
           </center>
           <form method="post" action="" id="Formulaire" name="Formulaire">
           <span id="missNom"></span>
-          <input type="text" name="nom" placeholder="Nom" id="nom" value="<?php echo $user['nom']?>"/>
+          <input type="text" name="nom" placeholder="Nom" value="<?php echo $user['nom'];?>" required id="nom">
           <span id="missPrenom"></span>
-          <input type="text" name="prenom" placeholder="Prénom" id="prenom" value="<?php echo $user['prenom']?>" />
+          <input type="text" name="prenom" placeholder="Prénom" id="prenom" value="<?php echo $user['prenom']?>" required>
           <span id="missAdresse"></span>
-          <input type="text" name="adresse" placeholder="Adresse" id="adresse" value="<?php echo $user['adresse_contact']?>"/>
+          <input type="text" name="adresse" placeholder="Adresse" id="adresse" value="<?php echo $user['adresse_contact']?>" required>
           <span id="missCp"></span>
-          <input type="text" name="cp" id="cp"   placeholder="Code Postal" value="<?php echo $user['cp_contact']?>"/>
+          <input type="text" name="cp" id="cp"   placeholder="Code Postal" value="<?php echo $user['cp_contact']?>" required>
           <span id="missVille"></span>
-          <input type="text" name="ville" id="ville" placeholder="Ville" value="<?php echo $user['ville_contact']?>"/>
+          <input type="text" name="ville" id="ville" placeholder="Ville" value="<?php echo $user['ville_contact']?>" required>
           <span id="missTel"></span>
-          <input type="text" name="tel" id ="tel" placeholder="Téléphone" value="<?php echo $user['telephone']?>"/>
+          <input type="text" name="tel" id ="tel" placeholder="Téléphone" value="<?php echo $user['telephone']?>" required>
           <span id="missMail"></span>
-          <input type="email" name="mail" id="email" placeholder="Email Address" value="<?php echo $user['mail']?>"/>
+          <input type="email" name="mail" id="email" placeholder="Email Address" value="<?php echo $user['mail']?>" required>
           <span id="missMdp"></span>
-          <input type="password" name="mdp" id="mdp" required title="6 caractères minimun en majuscule et minuscule et un caractère spécial"  id="passe" placeholder="Mot de passe" value=""/>
+          <input type="password" name="mdp" id="passe" required title="6 caractères minimun en majuscule et minuscule et un caractère spécial"   placeholder="Mot de passe" value=""/>
           <span id="missMdp2"></span>
-          <input type="password" name="mdp2" id="mdp2" required id="pass" placeholder="Confirmation de mot de passe"/>
-          <input type="submit" name="caseconditions" id="modifier" value="Modifier son profil" />
+          <input type="password" name="mdp2"  required id="pass" placeholder="Confirmation de mot de passe"/>
+          <input type="submit" name="caseconditions" id="inscrire" value="Modifier son profil" />
           </form>
     </div>
-
-    <script src="../javaScript/verificationFormulaire.js" type="text/javascript"></script>
 
 
 </div>
 
-<script src="../javaScript/verifProfil.js" type="text/javascript"></script>
+<script src="../javaScript/verificationFormulaire.js" type="text/javascript"></script>
 
 <?php include("footer.php") ?>
 
