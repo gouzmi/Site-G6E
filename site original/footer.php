32fde80eba1bee682cd56d1dfa52ab4abaed16eb
gouzmi <guillaume.dupont.rm@gmail.com>
2018-01-31 11:52:09 +0100

    supp site original

--- site original/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <ul>
-        <li>© HomeTech2017. Tous droits réservés</li>
-        <li>Localisation : 28 rue Notre-Dame des Champs, 75006, PARIS </li>
-        <li><a href="contact.php" class="link"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
-    </ul>
-</footer>
